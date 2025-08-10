24c3ae03601ba907977c79696b1bf367a1a40651
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-08-10 16:38:51 -0400

    Add Task class
    Adds the Task class which allows new todo's to be created, with name, dueDate, desc, and urgency values.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./styles.css";
 
-console.log("Testing");
+import Task from "./task.js";
+
+const task1 = new Task("Get eggs");
+task1.dueDate = "04/32"
+task1.urgency = "true";
+
+console.table(task1);
---
+++ src/task.js
@@ -0,0 +1,62 @@
+export default class Task {
+    constructor(name, dueDate, desc, urgency) {
+        this.name = name;
+        // this.dueDate = dueDate;
+        // this.desc = desc;
+        // this.urgency = urgency;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        if (typeof newName === `string`){
+            this._name = newName
+        }
+        else {
+            console.error("Must be a valid string");
+        }
+    }
+
+    get dueDate() {
+        return this._dueDate
+    }
+
+    set dueDate(newDate) {
+        if (typeof newDate === `string`) {
+            this._dueDate = newDate
+        }
+        else {
+            console.error("Must be a valid string");
+        }
+    }
+
+    get desc() {
+        return this._desc;
+    }
+
+    set desc(newDesc) {
+        if (typeof newDesc === `string`) {
+            this._desc = newDesc;
+        }
+        else {
+            console.error("Must be a valid string value")
+        }
+    }
+
+    get urgency() {
+        return this._urgency;
+    }
+
+    set urgency(status) {
+        if (status === `true` || status === `false`) {
+            this._urgency = status;
+        }
+        else {
+            console.error("Urgency status must be true or false");
+        }
+    }
+
+    
+}
