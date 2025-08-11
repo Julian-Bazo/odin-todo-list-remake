072b548869098d5644a0590250527e0972c26e8c
Julian-Bazo <Julian.Bazo27@gmail.com>
2025-08-10 20:26:30 -0400

    Add more commands to editTask
    Adds 5 commands to edit the name, date, description, urgency, or outright delete the whole task (that is selected).

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import "./styles.css";
 import Task from "./task.js";
 import List from "./list.js";
 
+// Reminder on setters and getters
+// const sampleTask = new Task("Tutorial");
+// console.log(sampleTask);
+// sampleTask.name = "Now different!";
+// console.log(sampleTask);
+
 const newList = new List("Standard List");
-newList.addTask("Grab eggs");
+newList.addTask("Grab eggs", "04/23", "", "false");
 newList.addTask("Buy some fish");
 
 
 console.table(newList);
 newList.showTasks();
-newList.selectTask(1);
+newList.editTask(1, "name", "No more eggs");
+newList.editTask(2, "date", "4/20");
+newList.editTask(2, "urgency", "true");
+newList.editTask(2, "describe", "A sample description");
+// newList.editTask(1, "delete");
+
+newList.showTasks();
--- src/list.js
+++ src/list.js
@@ -4,11 +4,21 @@ export default class List {
     constructor(name) {
         this.name = name;
         this.data = [];
-        this.selectedTask = this.data[0];
+        this.selectedTask;
     }
 
-    addTask(name) {
+    addTask(name, dueDate, desc, urgency) {
         const newTask = new Task(`${name}`);
+        if (dueDate !== undefined) {
+        newTask.dueDate = `${dueDate}`;
+        }
+        if (desc === "") {
+            newTask.desc = `Add extra information here`;
+        }
+        if (urgency !== undefined) {
+            newTask.urgency = `${urgency}`;
+        }
+        console.log(`New task made: ${newTask.name}, ${newTask.dueDate}`);
         this.data.push(newTask);
     }
 
@@ -16,10 +26,32 @@ export default class List {
         console.table(this.data);
     }
 
-    selectTask (num) {
+    editTask (num, action, value) {
         this.selectedTask = this.data[num - 1];
         console.log(this.selectedTask);
+
+        if (action === "name") {
+            this.selectedTask.name = value;
+        }
+        if (action === "date") {
+            this.selectedTask.dueDate = value;
+        }
+        if (action === "describe") {
+            this.selectedTask.desc = value;
+        }
+        if (action === "urgency") {
+            this.selectedTask.urgency = value;
+        }
+        if (action !== "name" && action !== "date" && action !== "describe" && action !== "urgency" && action !== "delete") {
+            console.error("Edit action must be a valid command. Check editTask in list.js for a list of valid commands!");
+        }
+        if (num > this.data.length) {
+            console.error("List Item does not exist");
+        }
+        if (action === "delete") {
+            this.data.splice(num - 1, 1);
+        }
     }
-    
+
 
 }
